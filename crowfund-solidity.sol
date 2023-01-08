c974d1d8641c4c5f25d0f570375ffa58094dd515
İsmet AKPINAR <ismetakpinar@192.168.1.74>
2023-01-08 13:37:25 +0300

    first commit

---
+++ crowfund-solidity.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract CrowdSource{
+     address public manager;
+     uint public MinimumContribution;
+     address[] public approvals;
